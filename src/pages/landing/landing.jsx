4c8fa3aa4f84408c52eff7183a4a826a35b2f74c
NadaBargougui <nadabargougui1@gmail.com>
2025-07-15 23:06:50 +0100

    navbar

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -2,7 +2,7 @@ const Landing = () => {
   return (
     <>
       {/* Navigation Bar */}
-      <nav className="fixed top-0 w-full z-40 bg-[#a5b7ff]">
+      <nav className="fixed top-0 w-full z-40 bg-[#a5b7ff]/80 rounded-b-2xl shadow-2xl">
         <div className="px-4">
           <div className="px-4 h-16 flex items-center">
             <a href="/" className="font-mono text-2xl font-bold text-white">
@@ -12,8 +12,9 @@ const Landing = () => {
         </div>
       </nav>
 
-      {/* Main Section with Background */}
-      <div className="relative min-h-screen w-full pt-16 overflow-hidden">
+      {/* Landing Section with Video Background */}
+      <div className="relative min-h-screen w-full pt-75 overflow-hidden">
+        {/* Background Video */}
         <video
           autoPlay
           muted
@@ -21,7 +22,7 @@ const Landing = () => {
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover z-0"
         >
-          <source src="/videos/landingVideo.mp4" type="video/mp4" />
+          <source src="assets/videos/landingVideo.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
