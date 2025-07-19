ca92510b600a1f36e8868d645118956311d3c903
NadaBargougui <nadabargougui1@gmail.com>
2025-07-19 17:04:26 +0100

    home sidebar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -14,6 +14,84 @@ const Home = () => {
           </Link>
         </div>
       </nav>
+
+      {/* sidebar Section */}
+      <div className="fixed left-0 p-6 w-72 bg-[#546fca]/60 rounded-b-2xl shadow-lg h-screen overflow-y-auto">
+        <ul className="space-y-4 mt-12 text-xl text-white font-semibold w-full">
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Sidi Bouzid
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Béja
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Ben Arous
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Bizerte
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Gabès
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Gafsa
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Jendouba
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Kairouan
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Kasserine
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Kebili
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Kef
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Mahdia
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Manouba
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Medenine
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Monastir
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Nabeul
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Sfax
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Ariana
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Siliana
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Sousse
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Tataouine
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Tozeur
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Tunis
+          </li>
+          <li className="hover:bg-white/60 hover:text-[#546fca] cursor-pointer w-full border border-white rounded-md px-4 py-3">
+            Zaghouan
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
